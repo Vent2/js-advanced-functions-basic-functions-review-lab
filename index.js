d7935b215603d0963556ae8fbb7b7a3eba724d6e
Gerald Ventura <ventura.c.gerald@gmail.com>
2021-09-14 01:26:21 -0400

    commit tree aec1e9e61b7665dbc75d774f49f43ab4b2cc8c6c

--- index.js
+++ index.js
@@ -1,1 +1,71 @@
 // Your code here
+//defines saturdayFun function declaration as specified
+// 1) function exists
+// 2) uses the default activity 'roller-skate' when no arguments are passed
+// 3) permits the default activity to be overriden
+function saturdayFun(activity = 'roller-skate') {
+  return (`This Saturday, I want to ${activity}!`);
+}
+
+// defines mondayWork function expression as specified
+// 4) function exists
+// 5) uses the default activity 'go to the office' when no arguments are passed
+// 6) permits the default activity to be overriden
+function mondayWork(activity = 'go to the office') {
+  return (`This Monday, I will ${activity}.`);
+}
+
+// defines wrapAdjective function according to the specification
+// 7) function exists
+// 8) when initialized with '*' creates a function that, when called, wraps an adjective in a highlight
+// 9) when initialized with '||' creates a function that, when called, wraps an adjective in a highlight
+// defines an object called Calculator
+function wrapAdjective(wrap = "*") {
+    return function(adj = "special") {
+        return `You are ${wrap}${adj}${wrap}!`
+    }
+}
+
+// 10) has a JavaScript Object called Calculator as a local variable
+// that has a function called add
+// 11) Calculator.add exists
+// 12) calculates 1 + 3
+// that has a function called subtract
+// 13) Calculator.subtract exists
+// 14) calculates 1 - 3
+// that has a function called multiply
+// 15) Calculator.multiply exists
+// 16) calculates 1 * 3
+// that has a function called divide
+// 17) Calculator.divide exists
+// 18) calculates 10 / 5
+const Calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    },
+    multiply: function(a, b) {
+        return a * b;
+    },
+    divide: function(a, b) {
+        return a / b;
+    }
+}
+
+// Defines a function called actionApplyer
+// 19) exists
+// receives two arguments: a starting integer and an array of functions
+// 20) returns the given starting point, unchanged, when the array is empty
+// 21) Given 13, returns 4 after being acted on by several functions
+
+function actionApplyer(startInteger, arrayOfFunctions) {
+    if (arrayOfFunctions.length === 0) {
+        return startInteger;
+    }
+    for(const functions of arrayOfFunctions) {
+        startInteger = functions(startInteger);
+    }
+    return startInteger;
+}
